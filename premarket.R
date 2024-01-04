7e1cac8141a5db154494ba7d2b6ffd4f58aca053
Ezekiel Adebayo Ogundepo <gbganalyst@gmail.com>
2024-01-04 16:52:57 +0100

    Update premarket.R

--- premarket.R
+++ premarket.R
@@ -12,14 +12,14 @@ closed_price <- function(stock) {
     scrap(glue("https://www.marketwatch.com/investing/stock/{stock}?mod=search_symbol"), node = ".u-semi")[1]
   }
 
-stocks <- c("aapl", "tsla", "meta", "achr", "joby", "rklb", "shop", "soun", "nvda", "smci", "nflx", "gtlb", "googl", "amzn", "pltr", "msft", "orcl", "arkk", "snow", "amd", "crm", "crwd", "path", "twlo", "baba", "adbe") 
+ticker <- c("aapl", "tsla", "meta", "achr", "joby", "rklb", "shop", "soun", "nvda", "smci", "nflx", "gtlb", "googl", "amzn", "pltr", "msft", "orcl", "arkk", "snow", "amd", "crm", "crwd", "path", "twlo", "baba", "adbe") 
 
-premarket_price <- map_chr(stocks, premkt_price) %>%
+premarket_price <- map_chr(ticker, premkt_price) %>%
     parse_number()
 
-closed_price <- map_chr(stocks, closed_price) %>%
+closed_price <- map_chr(ticker, closed_price) %>%
     parse_number()
 
-df <- tibble(stocks, closed_price, premarket_price, `gain/loss` = premarket_price - closed_price)
+df <- tibble(ticker, closed_price, premarket_price, `gain/loss` = premarket_price - closed_price)
 
 df %>% write_csv("market-watch.csv")
